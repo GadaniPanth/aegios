34b7f5204ab68815883ab4fe8e719d4d325c8d61
GadaniPanth <gadanipanth@gmail.com>
2025-05-06 17:17:44 +0530

    navbar completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'aegios';
+  private _isMenuOpen = false;
+  get isMenuOpen(): boolean {
+    return this._isMenuOpen;
+  }
+
+  set isMenuOpen(value: boolean) {
+    this._isMenuOpen = value;
+    document.body.style.overflow = value ? 'hidden' : '';
+  }
+
+  toggleMenu(e: MouseEvent){
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  isDropDownOpen: boolean = false;
+
+  toggleDropdown(){
+    this.isDropDownOpen = !this.isDropDownOpen;
+  }
+  isDropDownOpen2: boolean = false;
+
+  toggleDropdown2(){
+    this.isDropDownOpen2 = !this.isDropDownOpen2;
+  }
+
+  ngOnInit(){
+    document.addEventListener('keydown', (e)=>{
+      if(e.code.toLocaleLowerCase() == 'escape'){
+        this.isMenuOpen = false;
+      }
+    })
+  }
+
 }
