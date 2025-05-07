ec719318c48cdbc35ec5e835eeb44c710e102b6f
PARAS4105 <paras@coronation.in>
2025-05-07 18:24:18 +0530

    button added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AboutUsComponent } from "./about-us/about-us.component";
 import { QualityComponent } from "./quality/quality.component";
 import { SustainabilityComponent } from "./sustainability/sustainability.component";
 import { CareerComponent } from "./career/career.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
 
 const routes: Routes = [
   {
@@ -26,7 +27,11 @@ const routes: Routes = [
   {
     path: "career",
     component: CareerComponent,
-  }
+  },
+  {
+    path: "contact-us",
+    component: ContactUsComponent,
+  },
 ];
 
 @NgModule({
