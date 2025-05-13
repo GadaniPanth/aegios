b9ecdda8653485572fe2fd0a68f92d4527bf95e4
PARAS4105 <paras@coronation.in>
2025-05-13 15:54:52 +0530

    counter pending

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ViewChildren, QueryList, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.less']
 })
-export class AboutUsComponent implements OnInit {
+export class AboutUsComponent implements AfterViewInit {
+   @ViewChildren('counter') counters!: QueryList<ElementRef>;
 
   constructor() { }
 
   ngOnInit() {
   }
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries, obs) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const el = entry.target as HTMLElement;
+            const target = +(el.getAttribute('data-target') || '0');
+            let count = 0;
+            const increment = target / 80;
 
+            const updateCount = () => {
+              count += increment;
+              if (count < target) {
+                el.childNodes[0].nodeValue = Math.ceil(count).toString();
+                requestAnimationFrame(updateCount);
+              } else {
+                el.childNodes[0].nodeValue = target.toString();
+              }
+            };
+
+            updateCount();
+            obs.unobserve(el);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    this.counters.forEach(counter => {
+      observer.observe(counter.nativeElement);
+    });
+  }
 }
